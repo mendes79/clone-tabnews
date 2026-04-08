688d6702ae9b790bc57ae0bb1b35ab1a7a79364f
mendes79 <mendes79@gmail.com>
2026-04-08 12:32:24 -0300

    modifica a frase versão 2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-  return <h1>Do the evolution baby!</h1>;
+  return (
+    <h1>"Seja um adulto que deixaria orgulhosa a criança que você foi."</h1>
+  );
 }
 
 export default Home;
